88bc023df4f5cbbb7c9e9b3b5c1fb12910a9708f
Sherri Enoch <senoch@gmail.com>
2021-12-27 13:31:21 -0500

    updated images and control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
 
 interface Slide {
   id: number,
@@ -15,7 +15,7 @@ export class AppComponent  {
 
   slides: Slide[] = [{
     id: 1,
-    image: './assets/0.png',
+    image: './assets/1.png',
   }, {
     id: 2,
     image: './assets/2.png',
@@ -30,19 +30,44 @@ export class AppComponent  {
     image: './assets/5.png',
   }, {
     id: 6,
-    image: './assets/6.png',
+    image: './assets/fall.png',
   }, {
     id: 7,
-    image: './assets/7.png',
+    image: './assets/home.png',
   }, {
     id: 8,
-    image: './assets/8.png',
+    image: './assets/2-1.png',
   }, {
     id: 9,
-    image: './assets/9.png',
+    image: './assets/2-2.png',
   }, {
     id: 10,
-    image: './assets/10.png',
-  }]
+    image: './assets/2-3.png',
+  }, {
+    id: 11,
+    image: './assets/5-2.png',
+  }, {
+    id: 12,
+    image: './assets/5-2b.png',
+  }, {
+    id: 13,
+    image: './assets/2-5.png',
+  }, {
+    id: 14,
+    image: './assets/3-1.png',
+  }, {
+    id: 15,
+    image: './assets/3-2.png',
+  }, {
+    id: 16,
+    image: './assets/3-3.png',
+  }, {
+    id: 17,
+    image: './assets/3-3a.png',
+  }, {
+    id: 18,
+    image: './assets/3-4.png',
+  }
+]
 
 }
